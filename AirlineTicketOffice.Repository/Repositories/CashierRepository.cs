79741a283d8cb708e2dd7681fed0d57ce55b8de2
johnterrys <johnterryhfm93@gmail.com>
2017-10-18 19:35:41 +0300

    add cashierModel repo

---
+++ AirlineTicketOffice.Repository/Repositories/CashierRepository.cs
@@ -0,0 +1,31 @@
+﻿using AirlineTicketOffice.Model.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AirlineTicketOffice.Model.Models;
+using System.Linq.Expressions;
+using System.Data.Entity;
+using AirlineTicketOffice.Data;
+using System.Threading.Tasks;
+
+namespace AirlineTicketOffice.Repository.Repositories
+{
+    public sealed class CashierRepository : BaseModelRepository<Cashier>, ICashierRepository
+    {
+        public IEnumerable<CashierModel> GetAll()
+        {
+            return _context.Cashiers.AsNoTracking().ToArray().Select((Cashier c) =>
+            {
+                return new CashierModel
+                {
+                    CashierID = c.CashierID,
+                    NumberOfOffices = c.NumberOfOffices,
+                    FullName = c.FullName
+                };
+            });
+        }
+    }
+
+
+}
