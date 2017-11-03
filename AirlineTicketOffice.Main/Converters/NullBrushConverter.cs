6a410185b0946d55c23306603ff7c5553a54e4bd
johnterrys <johnterryhfm93@gmail.com>
2017-11-03 10:41:02 +0300

    add dirty hack 'nullbrushconverter'

---
+++ AirlineTicketOffice.Main/Converters/NullBrushConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace AirlineTicketOffice.Main.Converters
+{
+    public class NullBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value == null)
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+
+            BrushConverter bc = new BrushConverter();
+
+            return bc.ConvertFromInvariantString((String)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
